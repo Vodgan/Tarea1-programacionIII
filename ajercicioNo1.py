daedb91b8ffa65620808eb29c678964b2ad97260
Vodgan <vodganmendoza@gmail.com>
2021-03-11 22:20:52 -0600

    ejercicio 1 con comentario uno de git

--- ajercicioNo1.py
+++ ajercicioNo1.py
@@ -2,7 +2,7 @@ def ejercicio1():
     limite = 100
     indice=1
     indice2=0
-
+#comentario de prueba para cambio con git
     while indice <=limite:
         if indice % 2:
             print('Es impar - inidice = ' + str(indice))
